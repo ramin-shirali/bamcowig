31277a786ab3ffea3075d476aa75f6c6f0af366f
rshirali <rshirali@rshirali-haga-1.home>
2026-01-30 10:33:21 +0100

    Getting the chromosome names from the header

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod utils;
 use clap::Parser;
 use std::{env, path::{Path, PathBuf}};
 use crate::utils::bam_handler::bam_index_reader;
+use crate::utils::bam_handler::get_chromosome_names;
+use std::any::type_name;
+
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -12,11 +15,16 @@ struct Cli {
     bam_file_name: PathBuf,
 }
 
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
 fn main() {
     let args = Cli::parse();
     println!("{:?}", args);
-    let mut bam_index_file_name: PathBuf = PathBuf::from(format!("{}.bai", args.bam_file_name.display()));
+    let bam_index_file_name: PathBuf = PathBuf::from(format!("{}.bai", args.bam_file_name.display()));
     println!("{:#?}", bam_index_file_name);
     let bam_index = bam_index_reader(bam_index_file_name).unwrap();
-    println!("{:#?}", bam_index);
+    println!("{:#?}", get_chromosome_names(args.bam_file_name).unwrap());
 }
--- src/utils/bam_handler.rs
+++ src/utils/bam_handler.rs
@@ -1,8 +1,9 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, vec};
 use noodles_bam as bam;
 use noodles_sam as sam;
+use bstr;
 
-pub fn bam_header_reader(input_path: PathBuf) -> Result<sam::Header, std::io::Error>{
+fn bam_header_reader(input_path: PathBuf) -> Result<sam::Header, std::io::Error>{
     let mut reader = bam::io::reader::Builder::default().build_from_path(input_path)?;
     let header = reader.read_header()?;
     Ok(header)
@@ -13,4 +14,11 @@ pub fn bam_index_reader(input_path: PathBuf) -> Result<bam::bai::Index, std::io:
     let index = bam::bai::fs::read(input_path)?;
     // Ok::<_, io::Error>(())
     Ok(index)
+}
+
+pub fn get_chromosome_names(input_path: PathBuf) -> Result<Vec<bstr::BString>, std::io::Error>{
+    let header: sam::Header = bam_header_reader(input_path)?;
+    let chromosomes = header.reference_sequences().keys().cloned().collect();
+    // Ok::<_, io::Error>(())
+    Ok(chromosomes)
 }
