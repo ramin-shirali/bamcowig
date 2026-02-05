39e6211bc815c0760e418e66e4bfe51ff3c47456
rshirali <rshirali@rshirali-haga.home>
2026-02-05 19:10:40 +0100

    Count the total number of reads and add them to the Alignment struct, however it is slow, so maybe I remove it later

--- src/utils/alignment_handler.rs
+++ src/utils/alignment_handler.rs
@@ -1,11 +1,12 @@
+
 use std::path::PathBuf;
-use noodles_sam::Header;
 use noodles_util::alignment as noodles_alignment;
 use noodles_core::Region;
 
 pub struct Alignment{
     reader: noodles_alignment::io::IndexedReader<std::fs::File>,
     header: noodles_sam::Header,
+    total_reads: i32,
 }
 
 impl Alignment{
@@ -13,8 +14,18 @@ impl Alignment{
     pub fn new(alignment_path: PathBuf) -> Result<Self, Box<dyn std::error::Error>>{
         let mut builder = noodles_alignment::io::indexed_reader::Builder::default();
         let mut reader = builder.build_from_path(&alignment_path)?;
-        let header=  reader.read_header()?;
-        Ok(Alignment { reader, header })
+        let mut header=  reader.read_header()?;
+        let total_reads = Self::count_total_reads(&mut reader, &mut header)?;
+        let alignment = Alignment { reader, header, total_reads: total_reads};
+        Ok(alignment)
+    }
+
+    fn count_total_reads(reader: &mut noodles_alignment::io::IndexedReader<std::fs::File>, header: &noodles_sam::Header) -> Result<i32, Box<dyn std::error::Error>>{
+        let mut count = 0;
+        for _ in reader.records(header){
+            count += 1;
+        }
+        Ok(count)
     }
 
     pub fn get_region_coverage(&mut self, region: String) -> Result<i32, Box<dyn std::error::Error>>{
