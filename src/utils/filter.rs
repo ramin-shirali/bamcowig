7b1e4011d68c6947b15c0177f05bbf7a2d7cce7b
rshirali <rshirali@rshirali-haga.home>
2026-02-05 10:32:58 +0100

    Implemented mapping_quality filter.

--- src/utils/filter.rs
+++ src/utils/filter.rs
@@ -3,6 +3,7 @@
 use std::io::Error;
 
 use noodles_util::alignment::Record;
+use noodles_sam::alignment::Record as _;
 
 pub struct Filter{
     minimum_mapping_quality: i32,
@@ -16,7 +17,7 @@ pub struct Filter{
 impl Default for Filter{
     fn default() -> Filter {
         Filter {minimum_mapping_quality: 10, 
-            ignore_duplicates_flag:true, 
+            ignore_duplicates_flag:false, 
             strand_selection: true,
             secondary_alignment_skip: true,
             supplementary_alignment_skip: true
@@ -25,7 +26,43 @@ impl Default for Filter{
 }
 
 impl Filter{
-    pub fn apply(record: Record) -> Result<Record, Box<dyn std::error::Error>>{
-        Ok(record)
+    pub fn apply(&self, record: &Record) -> Result<bool, Box<dyn std::error::Error>> {
+        if self.minimum_mapping_quality > 0 && self.check_mapping_quality(record)? {
+            return Ok(true);
+        }
+        if !self.ignore_duplicates_flag && self.check_duplicate(record)? {
+            return Ok(true);
+        }
+        if self.strand_selection && self.check_strand_selection(record)? {
+            return Ok(true);
+        }
+        if self.secondary_alignment_skip && self.check_secondary_alignment(record)? {
+            return Ok(true);
+        }
+        if self.supplementary_alignment_skip && self.check_supplementary_alignment(record)? {
+            return Ok(true);
+        }
+        Ok(false)
+    }
+
+    fn check_mapping_quality(&self, record: &Record) -> Result<bool, Box<dyn std::error::Error>>{
+        let quality = record.mapping_quality()
+        .transpose()? //flips Option and Result from mapping_quality
+        .map(|mq| mq.get() as i32) // MappingQuality is a wrapper around u8, .get() extracts the u8, then cast to i32
+        .unwrap_or(0); // None means unmap. So 0 in that case to filter.         
+
+        Ok(quality >= self.minimum_mapping_quality)
+    }
+    fn check_duplicate(&self, record: &Record) -> Result<bool, Box<dyn std::error::Error>>{
+        Ok(true)
+    }
+    fn check_strand_selection(&self, record: &Record) -> Result<bool, Box<dyn std::error::Error>>{
+        Ok(true)
+    }
+    fn check_secondary_alignment(&self, record: &Record) -> Result<bool, Box<dyn std::error::Error>>{
+        Ok(true)
+    }
+    fn check_supplementary_alignment(&self, record: &Record) -> Result<bool, Box<dyn std::error::Error>>{
+        Ok(true)
     }
 }
