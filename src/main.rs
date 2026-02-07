f2e9ce0eed9e36c0d8f73389135f693bdb3e0968
rshirali <rshirali@rshirali-haga.home>
2026-02-07 19:58:17 +0100

    Writing the output to a file

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use crate::utils::bam_handler::{get_chromosome_names,
     get_chr_chunk_reads};
 use crate::utils::alignment_handler;
 use std::any::type_name;
-
+use std::fs;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -20,7 +20,11 @@ struct Cli {
     #[arg(short, long)]
     index_file_path: PathBuf,
     #[arg(long, default_value_t = 50)]
-    bin_size: i32,
+    bin_size: u16,
+    #[arg(short, long, default_value = "coverage_over_bins.bed")]
+    output_file: PathBuf,
+    #[arg(short, long, default_value_t = 8)]
+    threads: u16,
 }
 
 
@@ -34,14 +38,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let bam_file_path = args.bam_file_path;
     //let bam_index_file_name: PathBuf = PathBuf::from(format!("{}.bai", bam_file_path.display()));
     let bam_index_file = args.index_file_path;
-    let bin_size: usize = args.bin_size as usize;
+    let bin_size = args.bin_size;
     let chromosome_size_map = get_chromosome_size_map(&bam_file_path).unwrap();
     let mut alignment = alignment_handler::Alignment::from_bam(
         bam_file_path,  bam_index_file
     )?;
-    
-    
+    let coverage_over_bins_all_chromosomes = alignment.coverage_by_bin_all(bin_size)?;
+    write_output(args.output_file, coverage_over_bins_all_chromosomes)?;
     // println!("{:#?}", get_chromosome_names(args.bam_file_name).unwrap());
     // println!("{:#?}", get_chromosome_size_map(args.bam_file_name).unwrap());
+    Ok(())
+}
+
+fn write_output(output: PathBuf, coverage_over_bins_all_chromosomes: Vec<Vec<u32>>) -> Result<(), Box<dyn std::error::Error>>{
+    let mut content = String::new();
+    for (chr_idx, coverage_over_bins) in coverage_over_bins_all_chromosomes.iter().enumerate() {
+        for (bin_idx, &coverage_single_bin) in coverage_over_bins.iter().enumerate() {
+            content.push_str(&format!("{}\t{}\t{}\n", chr_idx, bin_idx, coverage_single_bin));
+        }
+    }
+    fs::write(output, content).expect("Should be able to write to `/foo/tmp`");
     Ok(())
 }
